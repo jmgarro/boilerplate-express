db978a9897f9f91edd2d47e37df7a2b2a274a4c7
José Martinez Garro <josemartinezgarro@gmail.com>
2025-04-22 15:36:38 -0300

    cambios de la segunda clase

--- myApp.js
+++ myApp.js
@@ -1,5 +1,29 @@
+require('dotenv').config();
 let express = require('express');
 let app = express();
+app.use("/public", express.static(__dirname + '/public'));
+app.use(function(req, res, next){
+    console.log(req.method+" "+req.path+" - "+req.ip);
+    next();
+});
+app.get("/now", function(req, res, next){
+    req.time=new Date().toString();
+    next();
+}, function(req, res){
+    res.json({"time": req.time});
+});
+app.get("/", function (req, res){
+    absolutePath = __dirname + '/views/index.html';
+    res.sendFile(absolutePath);
+});
+app.get("/json", function (req, res){
+    message="Hello json";
+    if(process.env.MESSAGE_STYLE=='uppercase'){
+        message=message.toUpperCase();
+    }
+    res.json({"message": message});
+});
+module.exports = app;
 
 
 
