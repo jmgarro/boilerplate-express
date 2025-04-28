51276d8dda727d93f48c0bc34ab9263d3a700f97
José Martinez Garro <josemartinezgarro@gmail.com>
2025-04-28 13:16:19 -0300

    ultimos cambios

--- myApp.js
+++ myApp.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
+let bodyParser = require('body-parser');
 let express = require('express');
 let app = express();
+app.use(bodyParser.urlencoded({extended: false}));
 app.use("/public", express.static(__dirname + '/public'));
 app.use(function(req, res, next){
     console.log(req.method+" "+req.path+" - "+req.ip);
@@ -16,6 +18,26 @@ app.get("/", function (req, res){
     absolutePath = __dirname + '/views/index.html';
     res.sendFile(absolutePath);
 });
+app.get("/:word/echo", function (req, res){
+    res.json({"echo": req.params.word});
+});
+
+app.route('/name')
+  .get((req, res) => {
+    const { first, last } = req.query;
+    if (!first || !last) {
+      return res.status(400).json({ error: 'Debe enviar ambos nombres' });
+    }
+    res.json({ name: `${first} ${last}` });
+  })
+  .post((req, res) => {
+    const { first, last } = req.body;
+    if (!first || !last) {
+      return res.status(400).json({ error: 'Debe enviar ambos nombres' });
+    }
+    res.json({ name: `${first} ${last}` });
+  });
+
 app.get("/json", function (req, res){
     message="Hello json";
     if(process.env.MESSAGE_STYLE=='uppercase'){
